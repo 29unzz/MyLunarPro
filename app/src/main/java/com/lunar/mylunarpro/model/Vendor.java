4de94a638858de30863efa6b0b70daaba90c463f
Rudy Gunawan <rudy.gunawan@ai.astra.co.id>
2017-06-22 15:25:22 +0700

    initial commit

---
+++ app/src/main/java/com/lunar/mylunarpro/model/Vendor.java
@@ -0,0 +1,16 @@
+package com.lunar.mylunarpro.model;
+
+/**
+ * Created by rudygu033840 on 6/22/2017.
+ */
+
+public class Vendor {
+    private String id;
+    private String name;
+    private String predicate;
+    private String description;
+    private String address;
+    private String email;
+    private String phoneNo;
+
+}
